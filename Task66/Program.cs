baef45fd4155824815044d08e9bf2e3be851f9db
Ekaterina <ekaterina_r21@mail.ru>
2022-05-25 19:50:47 +0300

    Рекурсия. Сумма элементов в промежутке

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.WriteLine("Введите значения M и N: ");
+int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+int SumRec(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    else return N + SumRec(M, N - 1);
+}
+Console.WriteLine(SumRec(M, N));
